b056c6959958d79a86e221708708d11de56d027f
Anil <anilcheguri97@gmail.com>
2024-12-07 06:58:52 +0530

    https://github.com/anilcheguri97/configServermain.git

---
+++ src/test/java/com/capstone/configserver/CapstoneConfigServer/CapstoneConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.configserver.CapstoneConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
